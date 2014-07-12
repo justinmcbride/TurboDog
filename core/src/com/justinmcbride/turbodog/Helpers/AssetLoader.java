dae003abc05f564e03ea0c33bbee27865cc6cc13
Justin <justin.mcbride.599z@gmail.com>
2014-07-12 00:26:13 -0600

    adding screens

--- core/src/com/justinmcbride/turbodog/Helpers/AssetLoader.java
+++ core/src/com/justinmcbride/turbodog/Helpers/AssetLoader.java
@@ -4,6 +4,15 @@ package com.justinmcbride.turbodog.Helpers;
  * Created by Justin on 7/12/2014.
  */
 public class AssetLoader {
+    //
+    // Variables for all of the assets
+    //
+
+
+    //
+    // Getters for all of the assets
+    //
+
 
     public static void initialLoad() {
 
