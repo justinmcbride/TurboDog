9cee5ac4a8bc1c522ffb65f5f7994945e2266d2d
Justin McBride <justin@dare599z.com>
2014-07-13 00:02:32 -0600

    add startup screen

--- core/src/com/justinmcbride/turbodog/TurboDog.java
+++ core/src/com/justinmcbride/turbodog/TurboDog.java
@@ -1,13 +1,8 @@
 package com.justinmcbride.turbodog;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.justinmcbride.turbodog.Helpers.AssetLoader;
-import com.justinmcbride.turbodog.Screens.MainMenuScreen;
+import com.justinmcbride.turbodog.Screens.Screen;
 import com.justinmcbride.turbodog.Screens.ScreenManager;
 
 public class TurboDog extends Game {
@@ -17,6 +12,7 @@ public class TurboDog extends Game {
 		System.out.println("Creating the game...");
         AssetLoader.initialLoad();
         ScreenManager.getInstance().initialize(this);
+        ScreenManager.getInstance().show(Screen.INTRO);
 	}
 
     @Override
