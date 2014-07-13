09f1d2e4b314ce248ea90b7853522c82819811eb
Justin <justin.mcbride.599z@gmail.com>
2014-07-13 01:34:55 -0600

    added fonts, created initial renderings

--- core/src/com/justinmcbride/turbodog/Helpers/AssetLoader.java
+++ core/src/com/justinmcbride/turbodog/Helpers/AssetLoader.java
@@ -1,5 +1,8 @@
 package com.justinmcbride.turbodog.Helpers;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
 /**
  * Created by Justin on 7/12/2014.
  */
@@ -7,14 +10,23 @@ public class AssetLoader {
     //
     // Variables for all of the assets
     //
-
+    private static BitmapFont fontRegular, fontShadow;
 
     //
     // Getters for all of the assets
     //
 
+    public static BitmapFont getFontRegular() {
+        return fontRegular;
+    }
+
+    public static BitmapFont getFontShadow() {
+        return fontShadow;
+    }
 
     public static void initialLoad() {
-
+        // Load the fonts
+        fontRegular = new BitmapFont(Gdx.files.internal("reg/ubuntuReg32.fnt"));
+        fontShadow = new BitmapFont(Gdx.files.internal("shadow/ubuntuShadow32.fnt"));
     }
 }
--- core/src/com/justinmcbride/turbodog/Screens/IntroScreen.java
+++ core/src/com/justinmcbride/turbodog/Screens/IntroScreen.java
@@ -1,18 +1,42 @@
 package com.justinmcbride.turbodog.Screens;
 
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.justinmcbride.turbodog.Helpers.AssetLoader;
 
 /**
  * Created by justin on 7/12/14.
  */
 public class IntroScreen implements com.badlogic.gdx.Screen {
-    public IntroScreen(Object data) {
+    private SpriteBatch spriteBatch;
+    private OrthographicCamera camera;
+    private float gameWidth, gameHeight;
+    private BitmapFont fontReg, fontShadow;
 
+    public IntroScreen(Object data) {
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+        gameWidth = 136;
+        gameHeight = screenHeight / (screenWidth / gameWidth);
+
+        spriteBatch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(true, 137, 204);
+
+        fontReg = AssetLoader.getFontRegular();
+        fontShadow = AssetLoader.getFontShadow();
     }
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        spriteBatch.begin();
+        fontReg.draw(spriteBatch, "Hello", 100, 100);
+        spriteBatch.end();
     }
 
     @Override
